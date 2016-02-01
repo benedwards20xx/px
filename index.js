c83537f41e4c81fb500802a1fa88e54edd1cd84c
Ben Edwards <ben@benedwardsweb.com>
2016-02-01 15:26:29 -0500

    Attempt host static app

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -15,6 +15,8 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-});
+});*/
 
-
+var app = require('express')();
+app.use(express.static('app'));
+app.listen(process.env.PORT);
