ec902eeacf9394ea833fcbee5e7f0dafdf4e7732
Ben Edwards <ben@benedwardsweb.com>
2016-02-01 15:31:34 -0500

    Attempt just simple host

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*var express = require('express');
+var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -6,17 +6,17 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
+//app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('app/index');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-});*/
+});
 
-var app = require('express')();
+/*var app = require('express')();
 app.use(express.static('app'));
-app.listen(process.env.PORT);
+app.listen(process.env.PORT);*/
