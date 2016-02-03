ba6c384575a87755f444faafc2c4a8d86fde3a71
Ben Edwards <ben@benedwardsweb.com>
2016-02-03 15:27:15 -0500

    Fix Heroku

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/app'));
 
+app.set('views', __dirname + '/app');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
