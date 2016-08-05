6e6ebc1771b561d7fe8a0286bae40bbfa8a4a8ae
Ben Edwards <ben@benedwardsweb.com>
2016-08-05 17:12:44 -0400

    'Fixin'

--- app/level.js
+++ app/level.js
@@ -1,155 +1,171 @@
-var creatures = {
-  spider: { symbol: 's', color: '#663300' },
-  bat: { symbol: 'b', color: '#003399' },
-  zombie: { symbol: 'z', color: '#66ff66' },
-  demon: { symbol: 'd', color: '#ff0000' },
-  blueSlime: { symbol: 'o', color: '#0000ff' }
-};
+// var creatures = {
+//   spider: { symbol: 's', color: '#663300' },
+//   bat: { symbol: 'b', color: '#003399' },
+//   zombie: { symbol: 'z', color: '#66ff66' },
+//   demon: { symbol: 'd', color: '#ff0000' },
+//   blueSlime: { symbol: 'o', color: '#0000ff' }
+// };
 
 var level = function() {
-  var player;
-  var enemies = [];
-  var curLevelNum = 0;
-  var defaultMinCaves = 3;
-  var defaultMaxCaves = 5;
-  var defaultMinEnemies = 3;
-  var defaultMaxEnemies = 7;
-  var gameOverReason = '';
   var levelData = [
     {
-      wallColor: '#3b3b3b',
-      floorColor: '#d9d9d9',
-      waterColor: '#0099ff',
-      backgroundColor: '#ffffff',
-      enemyTypes: [creatures.spider, creatures.blueSlime]
-    }
-    ,
-    {
-      wallColor: '#3b3b3b',
-      floorColor: '#b3b3b3',
-      waterColor: '#0000ff',
-      backgroundColor: '#d9d9d9',
-      enemyTypes: [creatures.spider, creatures.bat]
-    },
-    {
-      wallColor: '#3b3b3b',
-      floorColor: '#8c8c8c',
-      waterColor: '#0000ff',
-      backgroundColor: '#b3b3b3',
-      enemyTypes: [creatures.bat, creatures.zombie]
-    },
-    {
-      wallColor: '#3b3b3b',
-      floorColor: '#666666',
-      waterColor: '#ff6600',
-      backgroundColor: '#8c8c8c',
-      enemyTypes: [creatures.zombie, creatures.blueSlime]
-    },
-    {
-      wallColor: '#3b3b3b',
-      floorColor: '#404040',
-      waterColor: '#ff6600',
-      backgroundColor: '#666666',
-      enemyTypes: [creatures.demon, creatures.zombie]
+      // numRows: 3,
+      // numCols: 3,
+      rowData: [
+        [0],
+        [1],
+        [0]
+      ],
+      colData: [
+        [0],
+        [1],
+        [0]
+      ]
     }
   ];
-  var maxLevelNum = levelData.length - 1;
-  return {
-    clearPlayer: function() {
-      player = null;
-    },
-    getPlayer: function() {
-      return player;
-    },
-    initPlayerAtPos: function(col, row) {
-      if (typeof player != 'undefined' && player != null) {
-        player.updateSymbol(col, row, player.symbol, player.color);
-      } else {
-        player = new Player(col, row);
-      } 
-    },
-    updatePlayerHP: function(hp) {
-      player.updateHP(hp);
-    },
-    isEnemySymbol: function(symbol) {
-      for (var e in levelData[curLevelNum].enemyTypes) {
-        if(levelData[curLevelNum].enemyTypes[e].symbol == symbol) {
-          return true;
-        }
-      }
-      return false;
-    },
-    moveEnemiesAI: function() {
-      for (var x in enemies) {
-        enemies[x].updatePositionAI();
-      }
-    },
-    addEnemy: function(enemy) {
-      enemies.push(enemy);
-    },
-    removeEnemyAtPos: function(col, row) {
-      for (var x = 0; x < enemies.length; x++) {
-        if(enemies[x].col == col && enemies[x].row == row) {
-          enemies.splice(x, 1);
-        }
-      }
-    },
-    updateEnemyHPatPos: function(col, row, hp) {
-      for (var e in enemies) {
-        if(enemies[e].row == row && enemies[e].col == col) {
-          enemies[e].updateHP(hp);
-        }
-      }
-    },
-    getLevelNum: function() {
-      return curLevelNum;
-    },
-    updateLevelNum: function(num) {
-      curLevelNum = num >= maxLevelNum ? maxLevelNum : num;
-    },
-    getMaxLevelNum: function() {
-      return maxLevelNum;
-    },
-    getWallColor: function() {
-      return levelData[curLevelNum].wallColor;
-    },
-    getFloorColor: function() {
-      return levelData[curLevelNum].floorColor;
-    },
-    getWaterColor: function() {
-      return levelData[curLevelNum].waterColor;
-    },
-    getBackgroundColor: function() {
-      return levelData[curLevelNum].backgroundColor;
-    },
-    getEnemyTypes: function() {
-      return levelData[curLevelNum].enemyTypes;
-    },
-    getMinCaves: function() {
-      return defaultMinCaves + curLevelNum;
-    },
-    getMaxCaves: function() {
-      return defaultMaxCaves + curLevelNum;
-    },
-    getMinEnemies: function() {
-      return defaultMinEnemies + curLevelNum;
-    },
-    getMaxEnemies: function() {
-      return defaultMaxEnemies + curLevelNum;
-    },
-    startNewLevel: function() {
-      enemies = [];
-      map.init();
-      map.digMap();
-      map.placeWater();
-      map.digHallways();
-      map.populate();
-    },
-    getGameOverReason: function() {
-      return gameOverReason;
-    },
-    updateGameOverReason: function(reason) {
-      gameOverReason = reason;
-    }
-  };
+  // var player;
+  // var enemies = [];
+  // var curLevelNum = 0;
+  // var defaultMinCaves = 3;
+  // var defaultMaxCaves = 5;
+  // var defaultMinEnemies = 3;
+  // var defaultMaxEnemies = 7;
+  // var gameOverReason = '';
+  // var levelData = [
+  //   {
+  //     wallColor: '#3b3b3b',
+  //     floorColor: '#d9d9d9',
+  //     waterColor: '#0099ff',
+  //     backgroundColor: '#ffffff',
+  //     enemyTypes: [creatures.spider, creatures.blueSlime]
+  //   }
+  //   ,
+  //   {
+  //     wallColor: '#3b3b3b',
+  //     floorColor: '#b3b3b3',
+  //     waterColor: '#0000ff',
+  //     backgroundColor: '#d9d9d9',
+  //     enemyTypes: [creatures.spider, creatures.bat]
+  //   },
+  //   {
+  //     wallColor: '#3b3b3b',
+  //     floorColor: '#8c8c8c',
+  //     waterColor: '#0000ff',
+  //     backgroundColor: '#b3b3b3',
+  //     enemyTypes: [creatures.bat, creatures.zombie]
+  //   },
+  //   {
+  //     wallColor: '#3b3b3b',
+  //     floorColor: '#666666',
+  //     waterColor: '#ff6600',
+  //     backgroundColor: '#8c8c8c',
+  //     enemyTypes: [creatures.zombie, creatures.blueSlime]
+  //   },
+  //   {
+  //     wallColor: '#3b3b3b',
+  //     floorColor: '#404040',
+  //     waterColor: '#ff6600',
+  //     backgroundColor: '#666666',
+  //     enemyTypes: [creatures.demon, creatures.zombie]
+  //   }
+  // ];
+  // var maxLevelNum = levelData.length - 1;
+  // return {
+  //   clearPlayer: function() {
+  //     player = null;
+  //   },
+  //   getPlayer: function() {
+  //     return player;
+  //   },
+  //   initPlayerAtPos: function(col, row) {
+  //     if (typeof player != 'undefined' && player != null) {
+  //       player.updateSymbol(col, row, player.symbol, player.color);
+  //     } else {
+  //       player = new Player(col, row);
+  //     } 
+  //   },
+  //   updatePlayerHP: function(hp) {
+  //     player.updateHP(hp);
+  //   },
+  //   isEnemySymbol: function(symbol) {
+  //     for (var e in levelData[curLevelNum].enemyTypes) {
+  //       if(levelData[curLevelNum].enemyTypes[e].symbol == symbol) {
+  //         return true;
+  //       }
+  //     }
+  //     return false;
+  //   },
+  //   moveEnemiesAI: function() {
+  //     for (var x in enemies) {
+  //       enemies[x].updatePositionAI();
+  //     }
+  //   },
+  //   addEnemy: function(enemy) {
+  //     enemies.push(enemy);
+  //   },
+  //   removeEnemyAtPos: function(col, row) {
+  //     for (var x = 0; x < enemies.length; x++) {
+  //       if(enemies[x].col == col && enemies[x].row == row) {
+  //         enemies.splice(x, 1);
+  //       }
+  //     }
+  //   },
+  //   updateEnemyHPatPos: function(col, row, hp) {
+  //     for (var e in enemies) {
+  //       if(enemies[e].row == row && enemies[e].col == col) {
+  //         enemies[e].updateHP(hp);
+  //       }
+  //     }
+  //   },
+  //   getLevelNum: function() {
+  //     return curLevelNum;
+  //   },
+  //   updateLevelNum: function(num) {
+  //     curLevelNum = num >= maxLevelNum ? maxLevelNum : num;
+  //   },
+  //   getMaxLevelNum: function() {
+  //     return maxLevelNum;
+  //   },
+  //   getWallColor: function() {
+  //     return levelData[curLevelNum].wallColor;
+  //   },
+  //   getFloorColor: function() {
+  //     return levelData[curLevelNum].floorColor;
+  //   },
+  //   getWaterColor: function() {
+  //     return levelData[curLevelNum].waterColor;
+  //   },
+  //   getBackgroundColor: function() {
+  //     return levelData[curLevelNum].backgroundColor;
+  //   },
+  //   getEnemyTypes: function() {
+  //     return levelData[curLevelNum].enemyTypes;
+  //   },
+  //   getMinCaves: function() {
+  //     return defaultMinCaves + curLevelNum;
+  //   },
+  //   getMaxCaves: function() {
+  //     return defaultMaxCaves + curLevelNum;
+  //   },
+  //   getMinEnemies: function() {
+  //     return defaultMinEnemies + curLevelNum;
+  //   },
+  //   getMaxEnemies: function() {
+  //     return defaultMaxEnemies + curLevelNum;
+  //   },
+  //   startNewLevel: function() {
+  //     enemies = [];
+  //     map.init();
+  //     map.digMap();
+  //     map.placeWater();
+  //     map.digHallways();
+  //     map.populate();
+  //   },
+  //   getGameOverReason: function() {
+  //     return gameOverReason;
+  //   },
+  //   updateGameOverReason: function(reason) {
+  //     gameOverReason = reason;
+  //   }
+  // };
 }();
--- app/main.js
+++ app/main.js
@@ -1,18 +1,18 @@
 var font = 'monospace';
 var fontSize = 24;
 
-var floorSymbol  = '.';
-var wallSymbol   = '#';
-var waterSymbol  = '~';
-var playerSymbol = '@';
-var exitSymbol   = '=';
-var foodSymbol   = '%';
+// var floorSymbol  = '.';
+// var wallSymbol   = '#';
+// var waterSymbol  = '~';
+// var playerSymbol = '@';
+// var exitSymbol   = '=';
+// var foodSymbol   = '%';
 
 var menuBackgroundColor = '#000000';
 var menuFontColor = '#ffffff';
 
-var defaultNumCols = 48;
-var defaultNumRows = 24;
+var defaultNumCols = 3;
+var defaultNumRows = 3;
 var defaultWidth = defaultNumCols * fontSize * 0.6;
 var defaultHeight = defaultNumRows * fontSize + fontSize + 6;
 
@@ -25,8 +25,9 @@ var game = new Phaser.Game(
 );
 
 function create() {
-  game.state.add('startMenu', startMenu);
+  //game.state.add('startMenu', startMenu);
   game.state.add('play', play);
-  game.state.add('gameOver', gameOver);
-  game.state.start('startMenu');
+  //game.state.add('gameOver', gameOver);
+  //game.state.start('startMenu');
+  game.state.start('play');
 }
